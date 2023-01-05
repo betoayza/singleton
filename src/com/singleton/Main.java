ccdcd1f6b3467979134c289e23a7961d8d2de252
betoayza <aaayza@gmail.com>
2023-01-04 21:02:06 -0300

    Second commit

--- src/com/singleton/Main.java
+++ src/com/singleton/Main.java
@@ -3,8 +3,9 @@ package com.singleton;
 public class Main {
 
     public static void main (String []args){
-        Singleton singleton = Singleton.getInstance();
-        Singleton singleton2 = Singleton.getInstance();
-        System.out.println(singleton + "\n" + singleton2);
+        Singleton car1 = Singleton.getInstance();
+        Singleton car2 = Singleton.getInstance();
+        Singleton car3 = Singleton.getInstance();
+        System.out.println(car1 + "\n" + car2 + "\n" + car3);
     }
 }
